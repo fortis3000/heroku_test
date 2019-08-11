2b9605466c35a7121ec0ae4878b7529d7afa9dd6
Fortis3000 <fortis3000@gmail.com>
2019-08-11 21:00:59 +0500

    logging fixing

--- app.py
+++ app.py
@@ -18,12 +18,13 @@ from number import number_calc
 import logging
 import timber
 
+logging.basicConfig(level=logging.INFO)
 log_apikey = os.getenv('timber_apikey')
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 timber_handler = timber.TimberHandler(source_id='14767', api_key=log_apikey)
-# timber_handler.setLevel(logging.INFO)
+timber_handler.setLevel(logging.INFO)
 
 logger.addHandler(timber_handler)
 
